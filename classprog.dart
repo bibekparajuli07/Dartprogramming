c73916404e8e797f62b15b324421d901e41443ae
bibekparajuli07 <bibekparajuli29#gmail.com>
2025-09-02 19:23:34 +0545

    3rd commit

--- classprog.dart
+++ classprog.dart
@@ -1,11 +1,22 @@
 void main() {
-  var circle = Shape();
-  circle.display();
+  var shape = Shape();
+  shape.display();
+  var circle = Circle();
+  circle.area();
 }
 
 class Shape {
   int sides = 0;
   void display() {
     print("I am a shape with $sides sides.");
+  }
+}
+
+class Circle extends Shape {
+  int radius = 5;
+
+  void area() {
+    double area = 3.14 * radius * radius;
+    print("Area of the circle: $area and also there is $sides sides");
   }
 }
