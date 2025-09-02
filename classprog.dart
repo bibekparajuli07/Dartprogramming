6996e05954a66b32f9de76ca1af8b15ae5c6423e
bibekparajuli07 <bibekparajuli29#gmail.com>
2025-09-02 19:11:42 +0545

    second commit

--- classprog.dart
+++ classprog.dart
@@ -1,5 +1,11 @@
-void main() {}
+void main() {
+  var circle = Shape();
+  circle.display();
+}
 
 class Shape {
   int sides = 0;
+  void display() {
+    print("I am a shape with $sides sides.");
+  }
 }
