546bf1eabb17a6149e4813f86277f0d33a6d230a
bibekparajuli07 <bibekparajuli29#gmail.com>
2025-09-19 19:57:59 +0545

    8th commit

---
+++ pack&lin.dart
@@ -0,0 +1,10 @@
+import 'dart:math' as math;
+
+void main() {
+  var inp1 = 12;
+  var inp2 = 23;
+  var greater = math.max(inp1, inp2);
+  var smaller = math.min(inp1, inp2);
+  print(smaller);
+  print(greater);
+}
