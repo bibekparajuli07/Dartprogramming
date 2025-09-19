9698080a358e067a3e584c0f01ffb151be65c029
bibekparajuli07 <bibekparajuli29#gmail.com>
2025-09-19 16:11:51 +0545

    5th commit

---
+++ practice2.dart
@@ -0,0 +1,12 @@
+void main() {
+  ///removing duplicate number and sorting the list in ascending numberS
+  List<int> lists = [6, 3, 2, 1, 2, 4, 5, 6, 7, 9, 1, 2];
+  List<int> listNumber = [];
+  for (int list in lists) {
+    if (!listNumber.contains(list)) {
+      listNumber.add(list);
+      listNumber.sort();
+    }
+  }
+  print(listNumber);
+}
